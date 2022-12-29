c27b2033ea32493777334cf3139a9a3bd832449d
Anuza Simkhada <anuzasimkhada77@gmail.com>
2022-12-29 09:25:10 +0545

    changes in theme

---
+++ app/src/androidTest/java/SliderAdapter.java
@@ -0,0 +1,12 @@
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class SliderAdapter extends FragmentPagerAdapter {
+    public SliderAdapter(FragmentManager fm){super(fm);}
+    @NonNull
+    @Override
+
+    public Fragment getItem(int position){}
+
+}
